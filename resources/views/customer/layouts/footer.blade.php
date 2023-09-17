02785a30b3264570c37d479a9927cb5accdc1a0a
shahriar vaez <shahriar.dota@gmail.com>
2023-09-17 14:43:57 +0330

    update footer

--- resources/views/customer/layouts/footer.blade.php
+++ resources/views/customer/layouts/footer.blade.php
@@ -74,14 +74,14 @@
         </section>
         <section class="row my-5">
             <section class="col">
-                <section class="fw-bold">شرکت سلام کالا</section>
-                <section class="text-muted footer-intro">ما همواره تلاش می کنیم بهترین خدمات را به مشتریان آمازون ارائه کنیم. به شما کمک می کنیم بهترین انتخاب را داشته باشید و با اطمینان خاطر خرید را انجام بدهید و در کوتاه ترین زمان ممکن کالای خود را دریافت کنید. همچنین ما 24 ساعته در هفت روز هفته به مشتریان مان خدمات ارائه می دهیم. و 7 روز ضمانت برگشت برای تمامی کالاها داریم.</section>
+                <section class="fw-bold">شرکت سلم کالا</section>
+                <section class="text-muted footer-intro">ما همواره تلاش می کنیم بهترین خدمات را به مشتریان سلم کالا ارائه کنیم. به شما کمک می کنیم بهترین انتخاب را داشته باشید و با اطمینان خاطر خرید را انجام بدهید و در کوتاه ترین زمان ممکن کالای خود را دریافت کنید. همچنین ما 24 ساعته در هفت روز هفته به مشتریان مان خدمات ارائه می دهیم. و 7 روز ضمانت برگشت برای تمامی کالاها داریم.</section>
             </section>
         </section>
 
         <section class="row border-top pt-4">
             <section class="col">
-                <section class="text-muted footer-intro text-center">کلیه حقوق این وبسایت متعلق به شرکت آمازون می باشد.</section>
+                <section class="text-muted footer-intro text-center">کلیه حقوق این وبسایت متعلق به شرکت سلم کالا می باشد.</section>
             </section>
         </section>
     </section>
