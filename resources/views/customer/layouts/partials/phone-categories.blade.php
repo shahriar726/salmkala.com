6003be276eed02f61eafc8e8298e1460e59d7bab
shahriar vaez <shahriar.dota@gmail.com>
2023-09-19 18:08:48 +0330

    category search mobile done

--- resources/views/customer/layouts/partials/phone-categories.blade.php
+++ resources/views/customer/layouts/partials/phone-categories.blade.php
@@ -1,9 +1,8 @@
 @foreach ($categories as $category)
 <section class="sidebar-nav-item">
       <span class="sidebar-nav-item-title">
-
-        {{ $category->name }}
-
+      <a href="{{ route('customer.products', ['category' => $category->id,'search' => request()->search, 'sort' => request()->sort, 'min_price' => request()->min_price, 'max_price' => request()->max_price, 'brands' => request()->brands]) }}"
+         class="d-inline">{{ $category->name }}</a>
           @if($category->children->count() > 0)
               <i class="fa fa-angle-left"></i>
           @endif
